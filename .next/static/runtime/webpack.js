7b2f928844916c807094aa83c2c4bdf3fdd31392
Ian Campbell <iancam92@icloud.com>
2019-03-31 15:49:20 -0700

    resume data is updated, by manual means. One day I will figure out how to do this automatically

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6ef73d3c613d6d1f8326";
+/******/ 	var hotCurrentHash = "6670569a90c10bfe9f83";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
